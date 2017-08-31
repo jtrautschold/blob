b385ce09906ad24f3104d75cc8144c8620988998
jtrautschold <jan.trautschold@live.de>
2017-08-31 19:15:20 +0200

    Add files via upload

---
+++ blob4drop.js
@@ -0,0 +1,25 @@
+function base64toBlob(b64Data, contentType, sliceSize) {
+    contentType = contentType || '';
+    sliceSize = sliceSize || 512;
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+            byteNumbers[i] = slice.charCodeAt(i);
+        }
+        var byteArray = new Uint8Array(byteNumbers);
+        byteArrays.push(byteArray);
+    }
+    var blob = new Blob(byteArrays, {type: contentType});
+    return blob;
+};
+
+
+
+var xx = document.querySelectorAll("div.dropzoneCon");
+
+for (var i = 0; i < xx.length; i++) {div.dropzoneCon[data-dropzone='1']
+    xx[i].style.data-dropzone="1";
+}
